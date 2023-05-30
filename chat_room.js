3ed7b4806c3b99243d8245f4292edeabfc9d9679
Ifeoluwa1111 <120338373+Ifeoluwa1111@users.noreply.github.com>
2023-05-30 16:41:06 -0600

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1zLqCKzmY1lpp5Xv8bK9meUdAEM77_78",
+  authDomain: "class-test-92458.firebaseapp.com",
+  databaseURL: "https://class-test-92458-default-rtdb.firebaseio.com",
+  projectId: "class-test-92458",
+  storageBucket: "class-test-92458.appspot.com",
+  messagingSenderId: "930380847563",
+  appId: "1:930380847563:web:41ca853ebddb2759f47e4e",
+  measurementId: "G-Q3N28632NP"
+};
+
+
+firebase.initializeApp(firebaseConfig);
